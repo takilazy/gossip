7a03f4f323e963dd2607d7e70d2d5a951074d14e
takilazy <taki.lazy@gmail.com>
2020-01-11 21:46:00 +0300

    user validation added, login started

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/takilazy/gossip/controllers"
 	"net/http"
@@ -12,19 +13,20 @@ func main() {
 
 	router := mux.NewRouter()
 
+	router.Handle("/", http.FileServer(http.Dir(".web/")))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(".web/static/"))))
+
+
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 	router.HandleFunc("/api/user", controllers.Get).Methods("GET")
 
 
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" //localhost
-	}
 
 	fmt.Println(port)
 
-	err := http.ListenAndServe(":" + port, router)
+	err := http.ListenAndServe(":" + port, handlers.LoggingHandler(os.Stdout, router))
 	if err != nil {
 		fmt.Print(err)
 	}
